3757b73b8bbf21a923d3cf992a4c2f8453493078
khum <1241367422@qq.com>
2018-05-22 11:40:23 +0800

    first commit

---
+++ app/src/main/java/com/kotlin/khum/mobilesafe/global/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.kotlin.khum.mobilesafe.global;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <pre>
+ *     author : khum
+ *     time   : 2018/5/22
+ *     desc   :
+ * </pre>
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(attachLayoutRes());
+    }
+
+    protected abstract int attachLayoutRes();
+
+}
---
+++ app/src/main/java/com/kotlin/khum/mobilesafe/ui/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.kotlin.khum.mobilesafe.ui
+
+import com.kotlin.khum.mobilesafe.R
+import com.kotlin.khum.mobilesafe.global.BaseActivity
+
+/**
+ * <pre>
+ *     author : khum
+ *     time   : 2018/5/22
+ *     desc   :
+ * </pre>
+ */
+class SplashActivity: BaseActivity() {
+    override fun attachLayoutRes(): Int = R.layout.activity_splash
+
+}
