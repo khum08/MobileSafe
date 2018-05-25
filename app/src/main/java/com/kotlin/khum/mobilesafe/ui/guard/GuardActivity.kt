2cf8e3201b2e9ad3db31bba671680f1666cf8f71
khum <1241367422@qq.com>
2018-05-25 13:00:39 +0800

    add guard

---
+++ app/src/main/java/com/kotlin/khum/mobilesafe/ui/guard/GuardActivity.kt
@@ -0,0 +1,20 @@
+package com.kotlin.khum.mobilesafe.ui.guard
+
+import com.kotlin.khum.mobilesafe.global.BaseActivity
+
+/**
+ * <pre>
+ *     author : khum
+ *     time   : 2018/5/25
+ *     desc   :
+ * </pre>
+ */
+class GuardActivity:BaseActivity() {
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun attachLayoutRes(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
