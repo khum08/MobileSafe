3d196147812c6f4d986d2d5d33dcd9e90579825f
yuanzhenkun <yzhk5215>
2018-08-26 16:56:53 +0800

    yzk 8/26

---
+++ app/src/main/java/com/kotlin/khum/mobilesafe/global/App.java
@@ -0,0 +1,69 @@
+package com.kotlin.khum.mobilesafe.global;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+import me.ele.uetool.UETool;
+
+/**
+ * <pre>
+ *     author : yuanzhenkun
+ *     time   : 2018/8/26
+ *     desc   :
+ * </pre>
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            private int visibleActivityCount;
+            private int uetoolDismissY = -1;
+
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                visibleActivityCount++;
+                if (visibleActivityCount == 1 && uetoolDismissY >= 0) {
+                    UETool.showUETMenu(uetoolDismissY);
+                }
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                visibleActivityCount--;
+                if (visibleActivityCount == 0) {
+                    uetoolDismissY = UETool.dismissUETMenu();
+                }
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
